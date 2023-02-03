1e81ca3c4de1dfd9dc513eb482ea31aeb359ac80
Gabriele Canepa <git@gabrielecanepa.com>
2023-02-03 11:52:25 +0100

    Minor updates

--- tools/algoutils/copier/index.js
+++ tools/algoutils/copier/index.js
@@ -6,21 +6,27 @@ const sourceIndex = sourceClient.initIndex('<SOURCE_INDEX>')
 const targetClient = algoliasearch('<TARGET_APP_ID>', '<TARGET_API_KEY>')
 const targetIndex = targetClient.initIndex('<TARGET_INDEX>')
 
-;(async () => {
-  // Hits
-  const hits = []
-  await sourceIndex.browseObjects({ batch: batch => hits.push(batch) })
-  await targetIndex.saveObjects(hits)
+const run = async () => {
+  try {
+    // Hits
+    const hits = []
+    await sourceIndex.browseObjects({ batch: batch => hits.push(batch) })
+    await targetIndex.saveObjects(hits)
 
-  // Settings
-  const settings = await sourceIndex.getSettings()
-  await targetIndex.setSettings(settings)
+    // Settings
+    const settings = await sourceIndex.getSettings()
+    await targetIndex.setSettings(settings)
 
-  // Synonyms
-  const synonyms = await sourceIndex.browseSynonyms()
-  await targetIndex.saveSynonyms(synonyms)
+    // Synonyms
+    const synonyms = await sourceIndex.browseSynonyms()
+    await targetIndex.saveSynonyms(synonyms)
 
-  // Rules
-  const rules = await sourceIndex.browseRules()
-  await targetIndex.saveRules(rules)
-})()
+    // Rules
+    const rules = await sourceIndex.browseRules()
+    await targetIndex.saveRules(rules)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+run()
