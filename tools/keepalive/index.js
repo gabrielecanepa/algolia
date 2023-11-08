f07dccec5f786a3ab37d002127968db516a40fd1
Gabriele Canepa <git@gabrielecanepa.com>
2023-11-08 23:30:38 +0100

    Update keepalive tool

--- tools/keepalive/index.js
+++ tools/keepalive/index.js
@@ -1,33 +1,34 @@
 import algoliasearch from 'algoliasearch'
-import { config as configDotenv } from 'dotenv'
+import { config } from 'dotenv'
 
-configDotenv()
+config()
 
 const APP_IDS = process.env.ALGOLIA_APP_IDS.split(',')
 const API_KEYS = process.env.ALGOLIA_API_KEYS.split(',')
-const KEEPALIVE_INDEX_NAME = process.env.ALGOLIA_KEEPALIVE_INDEX_NAME || '_keepalive'
+const KEEPALIVE_INDEX = process.env.ALGOLIA_KEEPALIVE_INDEX || '_keepalive'
 
 const run = async () => {
-  for (const [i, appId] of APP_IDS.entries()) {
+  for (const appId of APP_IDS) {
     try {
       // Init the client and keepalive index.
-      console.log(`Initializing the temporary index ${KEEPALIVE_INDEX_NAME} on ${appId}...`)
+      console.info(`Initializing temporary index ${KEEPALIVE_INDEX} on ${appId}...`)
       const client = algoliasearch(appId, API_KEYS[APP_IDS.indexOf(appId)])
-      const index = client.initIndex(KEEPALIVE_INDEX_NAME)
+      const index = client.initIndex(KEEPALIVE_INDEX)
   
       // Execute an indexing operation by adding a dummy record.
-      console.log(`Executing a record operation by adding a dummy record...`)
+      console.info(`Executing a record operation...`)
       await index.saveObject({}, { autoGenerateObjectIDIfNotExist: true })
 
       // Execute a search operation as soon as the index is available.
-      console.log(`Executing a search operation...`)
+      console.info(`Executing a search operation...`)
       while (!await index.exists()) continue
+      await index.search('')
 
       // Delete the keepalive index.
-      console.log(`Executing an index operation by deleting the temporary index...`)
+      console.info(`Executing an indexing operation...`)
       await index.delete()
 
-      console.log(`Done! The application is ready to be used.`)
+      console.info(`Done! The application ${appId} is ready to be used.`)
     } catch (e) {
       throw new Error(e.message)
     }
